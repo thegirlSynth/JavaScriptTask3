48e9af09c95e0df017f9998714f30e620631524f
Codeflour <cynthianuchepro@gmail.com>
2020-04-23 02:49:50 +0100

    next

--- jsTask3.js
+++ jsTask3.js
@@ -1,4 +1,5 @@
 //I'm trying to fall in love with JS. Tough love. @kingabesh and @jeff, you'll pro'lly never see this, but I think you guys rock. That being said...
+
 let numberArray = [];
 
 
@@ -6,48 +7,51 @@ let a = "yu";
 let b = "gi";
 let c = "oh";
 
+function numberReplace(chosenNumber) {
 
-for (let i = 1; i <= parameterNumber; i++)
-{
-
-  if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0)
+  for (let i = 1; i <= chosenNumber; i++)
   {
-    numberArray.push(a, "-", b, "-", c);
-  }
 
-  if (i % 2 == 0 && i % 3 == 0)
-  {
-    numberArray.push(a, "-", b);
-  }
+    if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0)
+    {
+      numberArray.push(a, "-", b, "-", c);
+    }
 
-  else if (i % 2 == 0 && i % 5 == 0)
-  {
-    numberArray.push(a, "-", c);
-  }
+    if (i % 2 == 0 && i % 3 == 0)
+    {
+      numberArray.push(a, "-", b);
+    }
+
+    else if (i % 2 == 0 && i % 5 == 0)
+    {
+      numberArray.push(a, "-", c);
+    }
 
 
-  else if (i % 3 == 0 && i % 5 == 0)
-  {
-    numberArray.push(b, "-", c);
-  }
+    else if (i % 3 == 0 && i % 5 == 0)
+    {
+      numberArray.push(b, "-", c);
+    }
 
-  else if (i % 2 == 0)
-  {
-    numberArray.push(a);
-  }
+    else if (i % 2 == 0)
+    {
+      numberArray.push(a);
+    }
 
-  else if (i % 3 == 0)
-  {
-    numberArray.push(b);
-  }
+    else if (i % 3 == 0)
+    {
+      numberArray.push(b);
+    }
 
-  else
-  {
-    numberArray.push(i);
-  }
+    else
+    {
+      numberArray.push(i);
+    }
 
-};
+  };
+}
 
+numberReplace(100);
 
 console.log(numberArray);
 //console.log (i++);
